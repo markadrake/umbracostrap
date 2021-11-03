2e18fd3ea205dc37bab67030b9ee311202d4e30c
Mark Drake <markusdrake@gmail.com>
2021-11-03 00:10:32 -0400

    UI project using bootstrap and parcel added

---
+++ Umbracostrap.UI/src/index.js
@@ -0,0 +1,9 @@
+// Option 1
+// Import Bootstrap's complete JavaScript bundle and Popper.js dependency
+
+import * as bootstrap from "bootstrap";
+
+// Option 2
+// Import only the plugins we need
+
+//import { Dropdown as Dropdown, Offcanvas as Offcanvas } from "bopotstrap";
---
+++ readme.md
@@ -0,0 +1,11 @@
+# TLDR
+
+Umbraco v9.0.1
+
+Administrator:
+Username: `demo@umbraco.com`
+Password: `demo@umbraco.com`
+
+Database:
+Local SQL DB (Windows-only)
+
